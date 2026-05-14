305c3cb74e4fe1307a4d0cb27749d4e31cd22002
kshox❤️ <josepenacurihual@gmail.com>
2026-05-14 16:10:58 -0400

    erasing ingsgih

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import cashFlowRoutes from "./modules/cashflow/cash-flow.routes";
 import invoicesDueRoutes from "./modules/invoices/invoices.routes";
 import billsRoutes from "./modules/bills/bills.routes";
 import peopleCostsRoutes from "./modules/people-costs/people-costs.routes";
-import insightsRoutes from "./modules/insights/insights.routes";
+
 import authRoutes from "./auth/login.router";
 
 import { errorHandlerMiddleware } from "./core/errors/error-handler.middleware";
@@ -26,7 +26,7 @@ app.use("/api/cash-flow", cashFlowRoutes);
 app.use("/api/invoices-due", invoicesDueRoutes);
 app.use("/api/bills", billsRoutes);
 app.use("/api/people-costs", peopleCostsRoutes);
-app.use("/api/insights", insightsRoutes);
+
 app.use("/api/auth", authRoutes);
 app.use("/api/bills", billsRoutes);
 
--- src/modules/insights/insights.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-import { InsightsService } from "./insights.service";
-
-export class InsightsController {
-  static getAll(_req: Request, res: Response) {
-    res.json(InsightsService.getAll());
-  }
-}
